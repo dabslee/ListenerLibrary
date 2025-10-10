3914bdca67484de118eee61143adc58fd3eed779
Brandon Lee <brandon.sangmin.lee@gmail.com>
2025-10-10 10:31:48 -0400

    Modified display for storage limit

--- player/templatetags/player_extras.py
+++ player/templatetags/player_extras.py
@@ -20,5 +20,5 @@ def format_bytes(size_bytes):
     size_name = ("B", "KB", "MB", "GB", "TB")
     i = int(math.floor(math.log(size_bytes, 1024)))
     p = math.pow(1024, i)
-    s = round(size_bytes / p, 2)
+    s = round(size_bytes / p, 1)
     return f"{s} {size_name[i]}"
