ccfe408c243416bb4cdca9040630c2618d153203
Brandon Lee <brandon@planet.net>
2025-10-17 11:09:13 -0400

    Fixed issue with file names not being pre-filled by upload field

--- player/context_processors.py
+++ player/context_processors.py
@@ -1,8 +1,9 @@
 from .models import UserPlaybackState, Bookmark
+from .forms import BookmarkForm
 
 def global_context(request):
     if request.user.is_authenticated:
         state = UserPlaybackState.objects.select_related('track', 'playlist').get(user=request.user) if UserPlaybackState.objects.filter(user=request.user).exists() else None
         bookmarks = Bookmark.objects.filter(user=request.user).order_by('name')
-        return {'playback_state': state, 'bookmarks': bookmarks}
-    return {'playback_state': None, 'bookmarks': []}
+        return {'playback_state': state, 'bookmarks': bookmarks, 'bookmark_form': BookmarkForm(prefix="bookmarkform")}
+    return {'playback_state': None, 'bookmarks': [], 'bookmark_form': BookmarkForm(prefix="bookmarkform")}
