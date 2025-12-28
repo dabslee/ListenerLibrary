5789c971db935194008ad713c09fe02f8e1ad17e
Brandon Lee <brandon.sangmin.lee@gmail.com>
2025-12-28 15:52:05 -0500

    Modified formatting of transcription request button

--- player/admin.py
+++ player/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import UserProfile, Track, UserPlaybackState, PodcastProgress, Bookmark
+from .models import UserProfile, Track, UserPlaybackState, PodcastProgress, Bookmark, Transcript
 
 admin.site.register(Track)
 admin.site.register(UserPlaybackState)
 admin.site.register(PodcastProgress)
 admin.site.register(Bookmark)
 admin.site.register(UserProfile)
+admin.site.register(Transcript)
