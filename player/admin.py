dc109ed80fa0ed0d8cd5fc2369375236e9c299a1
Brandon Lee <brandon.sangmin.lee@gmail.com>
2026-01-21 10:08:13 -0500

    Added UserTrackLastPlayed to admin for viewing

--- player/admin.py
+++ player/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, Track, UserPlaybackState, PodcastProgress, Bookmark, Transcript
+from .models import UserProfile, Track, UserPlaybackState, PodcastProgress, Bookmark, Transcript, UserTrackLastPlayed
 
 admin.site.register(Track)
 admin.site.register(UserPlaybackState)
@@ -7,3 +7,4 @@ admin.site.register(PodcastProgress)
 admin.site.register(Bookmark)
 admin.site.register(UserProfile)
 admin.site.register(Transcript)
+admin.site.register(UserTrackLastPlayed)
